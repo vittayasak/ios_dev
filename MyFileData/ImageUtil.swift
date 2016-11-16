029ff90b17348bdfc9701fc67d4e6eab7b85810b
Training <training@Trainings-Mac.local>
2016-11-16 23:45:12 +0700

    ImageUtil

--- MyFileData/ImageUtil.swift
+++ MyFileData/ImageUtil.swift
@@ -23,7 +23,7 @@ class ImageUtil{
         }
     }
     
-    static func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+    private static func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
         URLSession.shared.dataTask(with: url) {
             (data, response, error) in
             completion(data, response, error)
