6ec08eeceb063bc8d7f599b1428ca3d69cf8efc9
Training <training@Trainings-Mac.local>
2016-11-17 10:54:57 +0700

    core data

--- MyCoreData/ViewController.swift
+++ MyCoreData/ViewController.swift
@@ -7,9 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    @IBAction func btnSaveCoreData(_ sender: Any) {
+        let context = getContext()
+        let path = NSTemporaryDirectory();
+        print("path: \(path)")
+        let person = NSEntityDescription.entity(forEntityName: "Person", in: context)
+        let tx = NSManagedObject(entity: person!, insertInto: context)
+        tx.setValue(2,forKey:"id")
+        tx.setValue("John", forKey: "name")
+        tx.setValue("08122222",forKey:"phone")
+        tx.setValue("john@gmail.com",forKey:"email")
+        
+        do{
+            try context.save()
+            print("-- saved!---")
+        }catch let err as Error{
+            print("error-> \(err)")
+        }
+        
+    }
+    
+    @IBAction func btnReadCoreData(_ sender: Any) {
+        let fetchRequest: NSFetchRequest<Person> = Person.fetchRequest()
+        do {
+            //go get the results
+            let searchResults = try getContext().fetch(fetchRequest)
+  
+            print ("num of results = \(searchResults.count)")
+
+            for p in searchResults as [NSManagedObject] {
+
+                print("\(p.value(forKey: "name"))")
+            }
+        } catch {
+            print("Error with request: \(error)")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
